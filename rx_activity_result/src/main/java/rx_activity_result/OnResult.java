f029ca0af883c592829c21d3db2b246982df1cc4
Victor <victor@refineriaweb.com>
2016-03-22 14:14:33 +0100

    updated version crop library fix options bundle config

--- rx_activity_result/src/main/java/rx_activity_result/OnResult.java
+++
@@ -1,9 +0,0 @@
-package rx_activity_result;
-
-import android.content.Intent;
-
-import java.io.Serializable;
-
-interface OnResult extends Serializable {
-    void response(int resultCode, Intent data);
-}
