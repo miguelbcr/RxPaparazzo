ce3dde8358e2bf59774feb65b2c852d877c8d282
Santiago Castro <santiago@xmartlabs.com>
2016-05-17 10:40:47 -0300

    Ask for camera permission

--- rx_paparazzo/src/main/java/com/fuck_boilerplate/rx_paparazzo/interactors/GrantPermissions.java
+++ rx_paparazzo/src/main/java/com/fuck_boilerplate/rx_paparazzo/interactors/GrantPermissions.java
@@ -34,11 +34,20 @@ public final class GrantPermissions extends UseCase<Void> {
     }
 
     @Override public Observable<Void> react() {
+        return requestPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE);
+    }
+
+    private Observable<Void> requestPermissions(String... permissionsString) {
         RxPermissions permissions = RxPermissions.getInstance(targetUi.activity());
-        return permissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
+        return permissions.request(permissionsString)
                  .flatMap(granted -> {
                      if (granted) return Observable.<Void>just(null);
                      throw new UserCanceledException();
                  });
+    }
+
+    public Observable<Void> reactAlsoWithCameraPermission() {
+        return requestPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA);
     }
 }
